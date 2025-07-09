026e1199ada6d030ecc67b1fce67b1d176552e78
RobertoCarrera <robertocarrera93@gmail.com>
2025-07-09 13:44:55 +0200

    Corregidas las clases que se mostraban y la info

--- src/app/components/cartera-info/cartera-info.component.ts
+++ src/app/components/cartera-info/cartera-info.component.ts
@@ -1,7 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cartera-info',
+  imports: [FormsModule, CommonModule],
   templateUrl: './cartera-info.component.html',
   styleUrls: ['./cartera-info.component.css']
 })
--- src/app/components/cartera-info/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './cartera-info.component';
