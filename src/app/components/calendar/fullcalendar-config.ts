80be0dae98ccb874648fc93d845bc8c40dbcd82a
RobertoCarrera <robertocarrera93@gmail.com>
2025-08-01 17:17:18 +0200

    Solucionado bug que permitía seleccionar franjas del calendario a usuarios

--- src/app/components/calendar/fullcalendar-config.ts
+++ src/app/components/calendar/fullcalendar-config.ts
@@ -70,45 +70,11 @@ export const FULLCALENDAR_OPTIONS: CalendarOptions = {
     { daysOfWeek: [1,2,3,4,5], startTime: '08:00', endTime: '13:00' },
     { daysOfWeek: [1,2,3,4,5], startTime: '16:00', endTime: '20:00' }
   ],
-  selectable: true,
-  selectMirror: true,
+  selectable: false,
+  selectMirror: false,
   selectOverlap: false,
-  selectConstraint: {
-    start: '08:00',
-    end: '20:00',
-  },
-  selectLongPressDelay: 0, // Permite seleccionar con tap corto en móvil
-  selectMinDistance: 0, // Permite seleccionar con el mínimo movimiento
-  selectAllow: (selectInfo) => {
-    const start = selectInfo.start;
-    const end = selectInfo.end;
-    const now = new Date();
-    // No permitir seleccionar entre 13:00 y 16:00
-    const startHour = start.getHours();
-    const endHour = end.getHours();
-    if ((startHour < 13 && endHour > 13) || (startHour >= 13 && startHour < 16)) {
-      return false;
-    }
-    // No permitir seleccionar en días pasados
-    if (start.setHours(0,0,0,0) < now.setHours(0,0,0,0)) {
-      return false;
-    }
-    return true;
-  },
-  // No sobrescribas el handler de select, deja el comportamiento por defecto para que se muestre el highlight
-
-  dateClick: function(info) {
-    const calendarApi = info.view.calendar;
-    if (isMobile) {
-      if (info.view.type === 'dayGridMonth' || info.view.type === 'timeGridThreeDay') {
-        calendarApi.changeView('timeGridDay', info.date);
-      }
-    } else {
-      if (info.view.type === 'dayGridMonth') {
-        calendarApi.changeView('timeGridWeek', info.date);
-      }
-    }
-  },
+  // Desactivamos cualquier selección de celdas
+  // Eliminamos el handler de dateClick para que no cambie la vista al hacer click en el fondo
   events: [], // Aquí puedes cargar tus reservas
   height: '100%',
   expandRows: true,
